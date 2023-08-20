67df0ef5b3745f6ae4173bd0b0ffa72b3f2a0377
jphillips <josh.phillips@fearnworks.com>
2023-08-19 19:10:11 -0500

    Add missing height attr

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -23,6 +23,7 @@ export default async function UserProfile({ params }: Props) {
 
       <Image
         width={300}
+        height={300}
         src={image ?? '/mememan.webp'}
         alt={`${name}'s profile`}
       />
